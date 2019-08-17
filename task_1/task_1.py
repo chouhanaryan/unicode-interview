52c58194f20b3bc2b637fa88b955d2fc86fefc51
Aryan Chouhan <chouhanaryan@yahoo.com>
2019-08-17 16:57:46 +0530

    Logic changed

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -1,5 +1,6 @@
-binary = '010,1111,1010,10010,100000,10100,110'.split(',')
-for st in binary:
-	n = int(st, 2)
-	if n%5 == 0:
-		print(st)
+bin = '010,1111,1010,10010,100000,10100,110'.split(',')
+f = []
+for st in bin:
+	if int(st, 2)%5 == 0:
+		f.append(st)
+print(','.join(f))
